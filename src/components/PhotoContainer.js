fdd7898a6eeabb1c2582ec89da001d0cc09b901b
DevMo-13 <moniquecinatl@gmail.com>
2020-06-23 10:15:14 -0700

    Updated loading

--- src/components/PhotoContainer.js
+++ src/components/PhotoContainer.js
@@ -1,16 +1,18 @@
 import React, {Component} from 'react';
 import Photo from './Photo';
+import Loading from './Loading'
 import NoResults from './NoResults';
 
 class PhotoContainer extends Component {  
-
     render() {
         const data = this.props.data;
         const topic = this.props.topic;
         const loading = this.props.loading;
         let photos;
     
-        if(data.length > 0) {
+        if(loading) {
+            photos = <Loading />
+        } else if (data.length > 0) {
             photos = data.map(photo => <Photo key={photo.id} farmId={photo.farm} serverId={photo.server} id={photo.id} secret={photo.secret} title={photo.title} />)
         } else {
             photos = <NoResults />
@@ -18,14 +20,10 @@ class PhotoContainer extends Component {
 
         return(
             <div className="photo-container">
-                <h2>{topic}</h2>
-                {
-                    (loading) 
-                    ? <h3>Loading...</h3> 
-                    : <ul>
-                        {photos}
-                      </ul>
-                }
+                <h2>{topic}</h2> 
+                <ul>
+                    {photos}
+                </ul>
             </div>
         );
     };
