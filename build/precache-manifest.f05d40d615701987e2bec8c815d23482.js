b653e8a94b18243ec4d8e92f0c63d72188ebb8d3
DevMo-13 <moniquecinatl@gmail.com>
2021-02-26 14:53:34 -0800

    Prod build

---
+++ build/precache-manifest.f05d40d615701987e2bec8c815d23482.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ba2042b07a4b68613f2da3d350d7282",
+    "url": "/react-gallery-app/index.html"
+  },
+  {
+    "revision": "ed7c5fe153ad8c57f225",
+    "url": "/react-gallery-app/static/css/main.8325ea2a.chunk.css"
+  },
+  {
+    "revision": "89885e757b1251c75e3c",
+    "url": "/react-gallery-app/static/js/2.ba824916.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-gallery-app/static/js/2.ba824916.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed7c5fe153ad8c57f225",
+    "url": "/react-gallery-app/static/js/main.9e7bf4d8.chunk.js"
+  },
+  {
+    "revision": "a1079c7527df93963f31",
+    "url": "/react-gallery-app/static/js/runtime-main.743a392b.js"
+  }
+]);
